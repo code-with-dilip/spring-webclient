0d85c8e87bc80abb8b3c47c3592532db727f694a
Dilip Sundarraj <dilip.sundarraj@target.com>
2019-08-16 06:13:16 -0500

    added the code to build the empoloyee app

--- employee-app/src/main/java/com/learnwebclient/constants/EmployeeConstants.java
+++ employee-app/src/main/java/com/learnwebclient/constants/EmployeeConstants.java
@@ -1,4 +1,10 @@
 package com.learnwebclient.constants;
 
 public class EmployeeConstants {
+
+    public static final String GET_ALL_EMPLOYEES_V1  ="/v1/allEmployees";
+    public static final String EMPLOYEE_BY_ID_V1 ="/v1/employee/{id}";
+    public static final String GET_EMPLOYEE_BY_NAME_V1  ="/v1/employeeName";
+    public static final String ADD_EMPLOYEE_V1  ="/v1/employee";
+
 }
